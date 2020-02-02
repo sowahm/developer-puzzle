1aca3a9726b734bd05a11f7b6cec03dd65897dc6
Michael Sowah <michaelsowah@Michaels-iMac.local>
2020-02-01 23:41:11 -0600

    Latest update

--- apps/stocks/src/environments/environment.prod.ts
+++ apps/stocks/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: true,
-  apiKey: '',
+  apiKey: 'sk_9e1459e92f304b6ba5bed3a518a7581f',
   apiURL: 'https://sandbox.iexapis.com'
 };
