1414be53f97da8d98ec63d2753e6e56a145d2125
Michael Sowah <michaelsowah@Michaels-iMac.local>
2020-02-04 17:31:47 -0600

    Task3 Custom Dates

--- libs/stocks/feature-shell/src/lib/stocks-feature-shell.module.ts
+++ libs/stocks/feature-shell/src/lib/stocks-feature-shell.module.ts
@@ -4,7 +4,10 @@ import {
   MatFormFieldModule,
   MatInputModule,
   MatSelectModule,
-  MatButtonModule
+  MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+  
 } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { SharedUiChartModule } from '@coding-challenge/shared/ui/chart';
@@ -22,7 +25,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
-    SharedUiChartModule
+    SharedUiChartModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   declarations: [StocksComponent]
 })
